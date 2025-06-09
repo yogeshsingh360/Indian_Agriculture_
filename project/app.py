cb338a31e310315ec9dcca5304af6398c56c7b95
yogeshsingh360 <yogeshrajputsingh000@gmail.com>
2025-06-09 16:05:57 +0530

    16:05 09-06-2025

--- project/app.py
+++ project/app.py
@@ -97,6 +97,6 @@ def get_districts():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    port = int(os.environ.get("PORT", 5000))  
     app.run(host="0.0.0.0", port=port, debug=True)
 
